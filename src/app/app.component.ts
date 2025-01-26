1624c8bdce997602a16c1bd439099e1b33571c06
750584 <srr66166@gmail.com>
2025-01-26 17:42:02 +0530

    update app layout to display random text and success message based on user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker'
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'typing';
+  randomText= faker.lorem.sentence();
+  enteredText = '';
+
+  
 }
